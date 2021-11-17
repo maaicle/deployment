26ca855627ec3f5c580dc22954528a607008efdb
Michael Hardcastle <maaicle@gmail.com>
2021-11-17 14:09:33 -0700

    added css path

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.css'));
+});
+
 const port = process.env.PORT || 4006;
 
 app.listen(port, () => {
